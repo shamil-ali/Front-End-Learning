d926f58778d0c30107e659fc264178f375c30d2b
shamil-ali <shamilsali@aol.co.uk>
2022-04-05 11:57:38 +0100

    Task 2 JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -23,3 +23,63 @@ console.error(`This is an error message`);
 console.log(`%c` + `some text`, `color: black; background-color: white; font-size: 30px; padding: 15px;`);
 
 console.log(`%c` + `red text` + `%c` + `blue text`, `color: red;`, `color: blue`);
+
+// VARIABLES and DATA TYPES
+
+/*
+		In JavaScript, data types are DYNAMIC and MUTABLE
+		-> They are decided at run time
+		-> Stored in memory as text
+*/
+
+/*
+		We declare variables with:
+		1. let   -> standard variable
+		2. const -> constant
+		3. var   -> the old way we don't use anymore
+
+		Naming standard is camelCase
+		$variableName
+		_myVariable
+*/
+
+let    variableOne    = `hello`;
+const  variableTwo    = 4;
+var    oldVariable    = true;
+
+//  DATA TYPES
+
+/*
+		In JS there are two 'types' of types
+		PRIMATIVES and OBJECTS
+
+		OBJECTS have one datatype        -> object
+		PRIMATIVES have everything else  -> boolean, number, string, symbol, null, bigint, undefined
+*/
+
+// PRIMATIVES
+
+let aBoolean     = false;
+let aNumber      = 10;
+let aString      = `text123123@!!!`;
+let aBigInt      = 9999999999999999n;
+let aSymbol      = Symbol(`description`);
+let aNull        = null;
+let anUndefined;
+
+// OBJECTS : Collection of Properties, Mutable, made up of Key : Value pairs
+
+let myObject = {
+		key : `value`,
+		anotherKey : 12345
+}
+console.log(myObject);
+
+// STRING CONCATENATION : Joining strings end to end (Memory intensive)
+let string1 = `41 * 12 = `;
+let result1 = 41 * 12;
+console.log(string1 + result1);
+
+// STRING INTERPOLATION : Uses 1 string only, uses string literals containing placeholders, sometimes called 'template literals'
+let string2 = `42 * 13 = ${42 * 13}`;
+console.log(string2);
---
+++ JavaScript/task02.js
@@ -0,0 +1,7 @@
+`use strict`
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+
+console.log(`The total bill is £${totalMoney}, The remaining amount of money to be paid is £ ${totalMoney-moneyPaidSoFar}`);
