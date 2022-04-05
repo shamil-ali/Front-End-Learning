86beb99cf9f4305a99d7a990df64577b30884eec
shamil-ali <shamilsali@aol.co.uk>
2022-04-05 14:07:42 +0100

    task push

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,19 +10,19 @@
 
 // let let = 1;
 
-console.log(`Hello World`);
+// console.log(`Hello World`);
 
-console.info(`This is info`);
+// console.info(`This is info`);
 
-console.warn(`This is a warning`);
+// console.warn(`This is a warning`);
 
-console.error(`This is an error message`);
+// console.error(`This is an error message`);
 
 // Styling the console
 
-console.log(`%c` + `some text`, `color: black; background-color: white; font-size: 30px; padding: 15px;`);
+// console.log(`%c` + `some text`, `color: black; background-color: white; font-size: 30px; padding: 15px;`);
 
-console.log(`%c` + `red text` + `%c` + `blue text`, `color: red;`, `color: blue`);
+// console.log(`%c` + `red text` + `%c` + `blue text`, `color: red;`, `color: blue`);
 
 // VARIABLES and DATA TYPES
 
@@ -43,9 +43,9 @@ console.log(`%c` + `red text` + `%c` + `blue text`, `color: red;`, `color: blue`
 		_myVariable
 */
 
-let    variableOne    = `hello`;
-const  variableTwo    = 4;
-var    oldVariable    = true;
+// let    variableOne    = `hello`;
+// const  variableTwo    = 4;
+// var    oldVariable    = true;
 
 //  DATA TYPES
 
@@ -59,27 +59,135 @@ var    oldVariable    = true;
 
 // PRIMATIVES
 
-let aBoolean     = false;
-let aNumber      = 10;
-let aString      = `text123123@!!!`;
-let aBigInt      = 9999999999999999n;
-let aSymbol      = Symbol(`description`);
-let aNull        = null;
-let anUndefined;
+// let aBoolean     = false;
+// let aNumber      = 10;
+// let aString      = `text123123@!!!`;
+// let aBigInt      = 9999999999999999n;
+// let aSymbol      = Symbol(`description`);
+// let aNull        = null;
+// let anUndefined;
 
 // OBJECTS : Collection of Properties, Mutable, made up of Key : Value pairs
 
-let myObject = {
-		key : `value`,
-		anotherKey : 12345
-}
-console.log(myObject);
+// let myObject = {
+// 		key : `value`,
+// 		anotherKey : 12345
+// }
+// console.log(myObject);
 
 // STRING CONCATENATION : Joining strings end to end (Memory intensive)
-let string1 = `41 * 12 = `;
-let result1 = 41 * 12;
-console.log(string1 + result1);
+
+// let string1 = `41 * 12 = `;
+// let result1 = 41 * 12;
+// console.log(string1 + result1);
 
 // STRING INTERPOLATION : Uses 1 string only, uses string literals containing placeholders, sometimes called 'template literals'
-let string2 = `42 * 13 = ${42 * 13}`;
-console.log(string2);
+
+// let string2 = `42 * 13 = ${42 * 13}`;
+// console.log(string2);
+
+//  ASI : Automatic Semi-Colon Insertion
+
+/*
+		JS automatically inserts semi-colons for us at the end of a 'line'
+
+		This can lead to unwanted behaviour
+*/
+
+// let a = 1;
+// let b = 2;
+
+// a
+// ++
+// b
+
+// console.log(`a = ${a}, b = ${b}`);
+
+// let c = 1;
+// let d = 2;
+
+// c++;
+// d;
+
+// console.log(`c = ${c}, d = ${d}`);
+
+//  ITERATION
+
+//  loops
+
+//  FOR
+/*
+		use a counter and run until a condition is NO LONGER met
+*/
+
+// for(let i = 0; i < 5; i++) {
+// 		console.log(`i = ${i}`);
+// }
+
+//  WHILE
+/*
+		Work with booleans and run until a boolean is false
+*/
+
+// let condition = true;
+// let increment = 0;
+
+// while(condition) {
+// 		console.log(`increment me`);
+// 		increment++;
+
+// 		if(increment == 3) {
+// 				condition = false;
+// 		}
+// }
+
+//  DO-WHILE
+/*
+		Like a while loop but always runs at least once
+*/
+
+// let x = false;
+
+// do {
+// 		console.log(`We are running!`);
+// } while(x);
+
+// 	SWITCH CASES
+/*
+		Run until BREAK or RETURN
+*/
+
+// let num = 5;
+
+// switch(num) {
+// 	case 0:
+// 			console.log(`num = ${num}`);
+// 			break;
+// 	case 1:
+// 			console.log(`num = ${num}`);
+// 			break;
+// 	case 2:
+// 			console.log(`num = ${num}`);
+// 			break;
+// 	case 3:
+// 			console.log(`num = ${num}`);
+// 			break;
+// 	case 4:
+// 			console.log(`num = ${num}`);
+// 			break;
+// 	case 10:
+// 			console.log(`num = ${num}`);
+// 			break;
+// 	default:
+// 			console.log(`This is out of Scope`);
+// }
+
+let i = 2;
+while(i <= 6) {
+		console.log(i + 1);
+		i = i + 2;
+}
+
+for(let i = 2; i <= 6; i = i + 2) {
+		console.log(i + 1);
+}
---
+++ JavaScript/task03.js
@@ -0,0 +1,31 @@
+`use strict`
+
+// Exercise 1
+
+let A = 100;
+
+while(A < 200) {
+  A++;
+  console.log(`A = ${A}`)
+}
+
+// Exercise 2
+
+let B = 100;
+
+while(B <= 200) {
+  if(B % 2 == 0) {
+    console.log(`-`);
+  } else { console.log(`*`);
+    } B++;
+}
+
+// Exercise 3
+
+let i = 0;
+
+for (let j = 1; j <= 10; j++){
+while(i < 10) {
+  i++;
+  console.log(`${i}`);
+}console.log(`${j}`); }
